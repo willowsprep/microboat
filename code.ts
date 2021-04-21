c2202735114372ec47775cecd2cc5d8bb3dabd02
polygon <hi@parthiv.dev>
2021-04-21 14:16:24 -0700

    made it faster & not as high pitched

--- code.ts
+++ code.ts
@@ -4,12 +4,9 @@
  * @license MIT
  */
 
-
-
-
 console.log("hello"); console.log("initialising");
 music.playTone(Note.C, music.beat())
 
-for(let i = 1; i < 392; i++){
-    music.playTone(i + 440, 50)
+for(let i = 1; i < 300; i++){
+    music.playTone(i + 440, 30)
 }
