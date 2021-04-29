2636ddeb6963060f3d80dcd2816ee0b202cf0808
AI-of-Awesomeness <83036802+AI-of-Awesomeness@users.noreply.github.com>
2021-04-29 13:15:18 -0700

    Update and rename remoteTurnRight.ts to RemoteTurnRight.ts

--- RemoteTurnRight.ts
+++ RemoteTurnRight.ts
@@ -5,18 +5,16 @@
  * @license MIT
  */
 // Radio message config
+
 enum RadioMessage {
     message1 = 49434,
-    message2 = 1435,
-    message3 = 31126,
-    TurnLeft = 34166,
-    TurnRight = 37380,
+    LeftOn_Straight = 48393,
+    LeftOff_RightTurn = 58875,
     FailedMessage = 65518
 }
 let DebugMode = 0
 let Pitch = 0
-// top secret group
-radio.setGroup(2203)
+radio.setGroup(0)
 radio.setTransmitPower(7)
 basic.showIcon(IconNames.Skull)
 basic.forever(function () {
@@ -24,7 +22,7 @@ basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         DebugMode = 0
         if (DebugMode == 0) {
-            radio.sendMessage(RadioMessage.TurnLeft)
+            radio.sendMessage(RadioMessage.LeftOff_RightTurn)
         } else {
             radio.sendNumber(1)
         }
@@ -38,7 +36,7 @@ basic.forever(function () {
     } else if (input.buttonIsPressed(Button.B)) {
         DebugMode = 0
         if (DebugMode == 0) {
-            radio.sendMessage(RadioMessage.TurnRight)
+            radio.sendMessage(RadioMessage.LeftOn_Straight)
         } else {
             radio.sendNumber(2)
         }
