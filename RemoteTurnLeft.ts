caa995b46afceea4585287ebfbab3ab89d967ea6
AI-of-Awesomeness <83036802+AI-of-Awesomeness@users.noreply.github.com>
2021-04-30 10:22:43 -0700

    Create RemoteTurnLeft.ts
    other way turning code.

---
+++ RemoteTurnLeft.ts
@@ -0,0 +1,57 @@
+enum RadioMessage {
+    message1 = 49434,
+    RightOff_LeftTurn = 17706,
+    RightOn_Straight = 19294,
+    FailedMessage = 65518
+}
+let DebugMode = 0
+let Pitch = 0
+radio.setGroup(0)
+radio.setTransmitPower(7)
+basic.showIcon(IconNames.Skull)
+basic.forever(function () {
+    Pitch = input.acceleration(Dimension.Y)
+    if (input.buttonIsPressed(Button.A)) {
+        DebugMode = 0
+        if (DebugMode == 0) {
+            radio.sendMessage(RadioMessage.RightOff_LeftTurn)
+        } else {
+            radio.sendNumber(10)
+        }
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (input.buttonIsPressed(Button.B)) {
+        DebugMode = 0
+        if (DebugMode == 0) {
+            radio.sendMessage(RadioMessage.RightOn_Straight)
+        } else {
+            radio.sendNumber(20)
+        }
+        basic.showLeds(`
+            # # . # #
+            # # # # #
+            . # . # .
+            # # # # #
+            # # . # #
+            `)
+    } else {
+        DebugMode = 0
+        if (DebugMode == 0) {
+            radio.sendMessage(RadioMessage.FailedMessage)
+        } else {
+            radio.sendNumber(30)
+        }
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    }
+})
